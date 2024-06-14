9000f33016be14bf7a551b6b7c10e7aca292daac
Nils <nise00001@stud.uni-saarland.de>
2024-06-14 17:43:00 +0200

    Division first attempt

--- src/main/scala/arithmetic/Divider.scala
+++ src/main/scala/arithmetic/Divider.scala
@@ -16,8 +16,27 @@ class Divider(bitWidth: Int) extends Module {
     val remainder = RegInit(0.U(bitWidth.W))       //current remainder
     val quotient = RegInit(VecInit(Seq.fill(bitWidth)(0.U(1.W))))   //= {dividend[i:0], quotient[N−1:i+1]}, where dividend is the input dividend and quotient is the final output quotient, and i is the current cycle
     val divisor = RegInit(0.U(bitWidth.W))         //divisor
+    val clock = RegInit(0.U(bitWidth.W))
     
-    for (i <- bitWidth - 1 to 0 by -1) {
-        //gaming holy moly
-    } 
+    when(io.start){
+        clock := bitWidth.U - 1.U
+        remainder := 0.U
+        divisor := io.divisor
+        quotient(clock) := dividend(clock)
+    }.otherwise{
+        clock := clock - 1.U
+        val r := (remainder << 1) + dividend(clock)
+        when(r < divisor){
+            quotient(clock) := 0
+            remainder := r
+        }.otherwise{
+            quotient(clock) := 1
+            remainder := r - divisor
+        }
+        when(clock === 0){
+            io.quotient := quotient
+            io.remainder := remainder
+            io.done := true.B
+        }
+    }
 }
